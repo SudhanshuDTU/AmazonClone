6e8503001efb70292cea6d1cc0007042a1d94adb
Sudhanshu Jha <Sudhanshujha2717@gmail.com>
2023-08-04 19:48:18 +0530

    day2

---
+++ lib/features/auth/auth_services/auth_service.dart
@@ -0,0 +1,11 @@
+import 'package:http/http.dart' as http;
+
+class AuthService {
+  //signup user
+  void signupUser(
+      {required String name,
+      required String email,
+      required String password}) async {
+    try {} catch (e) {}
+  }
+}
